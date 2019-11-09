35244a3d3cffeccbe609ad60361ac5448e787fd0
hyp3rflow <hyperflow@korea.ac.kr>
2019-11-09 16:43:59 +0900

    River Crossing Problem

---
+++ Algorithm/RiverCrossing/pwgc.c
@@ -0,0 +1,165 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define PEASANT 0x08
+#define WOLF 0x04
+#define GOAT 0x02
+#define CABBAGE 0x01
+
+// 인접행렬로 표현된 graph를 화면에 출력
+void print_graph(int graph[][16], int num){
+	
+}
+
+// 주어진 상태 state의 마지막 4비트를 파일에 출력 (화면 출력은 stdout)
+void bit_print(FILE *fp, int state);
+
+// 주어진 상태 state에서 농부, 늑대, 염소, 양배추의 상태를 각각 추출하여 p, w, g, c에 저장
+void get_pwgc(int state, int *p, int *w, int *g, int *c);
+
+// 주어진 그래프(graph)를 .net 파일로 저장
+// pgwc.net 참조
+void save_graph(char *filename, int graph[][16], int num);
+
+// 허용되지 않는 상태인지 검사
+// 예) 농부없이 늑대와 염소가 같이 있는 경우 / 농부없이 염소와 양배추가 같이 있는 경우
+// return value: 1 허용되지 않는 상태인 경우, 0 허용되는 상태인 경우
+int is_dead_end(int n) {
+	// 농부 없이 늑대-염소 or 농부 없이 염소-양배추 -> 허용 안되면 1
+	int p, w, g, c;
+	p = n & PEASANT ? 1 : 0;
+	w = n & WOLF ? 1 : 0;
+	g = n & GOAT ? 1 : 0;
+	c = n & CABBAGE ? 1 : 0;
+
+	// 늑대-염소인 경우 -> 농부 없는 경우
+	if (w == g && w != p) {
+		return 1;
+	} else if (g == c && g != p) {
+		return 1;
+	}
+
+	return 0;
+}
+
+// state1 상태에서 state2 상태로의 전이 가능성 점검
+// 농부 또는 농부와 다른 하나의 아이템이 강 반대편으로 이동할 수 있는 상태만 허용
+// return value: 1 전이 가능한 경우, 0 전이 불이가능한 경우
+int is_possible_transition(int s1, int s2) {
+    // 강 반대편으로 넘어갈 수 있는 경우 생각
+    // 일단 두 상태의 농부의 위치는 달라야 한다.
+    // 다음으로는 아이템의 위치가 최대 1개만 바뀌어야 한다.
+    int p, w, g, c;
+    p = (s1 ^ s2) & PEASANT;
+    w = (s1 ^ s2) & WOLF;
+    g = (s1 ^ s2) & GOAT;
+    c = (s1 ^ s2) & CABBAGE;
+
+    if (p) {
+        int chk = w ^ g ^ c;
+        if (w == g == c == 0) {
+            return 1;
+        } else if (w == g == c == 1) {
+            return 0;
+        } else if (chk) {
+            return 1;
+        }
+    }
+	else return 0;
+}
+
+// 상태들의 인접 행렬을 구하여 graph에 저장
+// 상태간 전이 가능성 점검
+// 허용되지 않는 상태인지 점검
+void make_adjacency_matrix(int graph[][16]);
+
+// 상태 변경: 농부 이동
+// return value : 새로운 상태
+int changeP(int state){
+	int p = state & PEASANT;
+
+	if(p) return state - PEASANT;
+	else return state + PEASANT;	
+}
+
+// 상태 변경: 농부, 늑대 이동
+// return value : 새로운 상태
+int changePW(int state){
+	int newState = changeP(state);
+	int w = newState & WOLF;
+
+	if(w) return newState - WOLF;
+	else return newState + WOLF;
+}
+
+// 상태 변경: 농부, 염소 이동
+// return value : 새로운 상태
+int changePG(int state){
+	int newState = changeP(state);
+	int g = newState & GOAT;
+
+	if(g) return newState - GOAT;
+	else return newState + GOAT;
+}
+
+// 상태 변경: 농부, 양배추 이동
+// return value : 새로운 상태
+int changePC(int state){
+	int newState = changeP(state);
+	int c = newState & CABBAGE;
+
+	if(CABBAGE) return newState - CABBAGE;
+	else return newState + CABBAGE;
+}
+
+// 방문한 상태인지 검사
+// return value : 1 visited, 0 not visited
+int is_visited(int *visited, int level, int state);
+
+// 방문한 상태들을 차례로 화면에 출력
+void print_states(int *visited, int count);
+
+// recursive function
+void dfs_main(int state, int goal_state, int level, int *visited){
+	if(state == goal_state || visited[state]){
+		visited[state] = level;
+		return;
+	}
+	else{
+		int newState;
+		visited[state] = level;
+		int (*fp[4])(int) = {changeP, changePC, changePG, changePW};
+		for(int i=0; i<4; i++){
+			newState = fp[i](state);
+			if(is_possible_transition(state, newState) && !is_dead_end(newState) && !is_visited(visited, level, newState)){
+				dfs_main(newState, goal_state, level+1, visited);
+			}
+		}
+	}
+}
+
+// 깊이 우선 탐색 (초기 상태 -> 목적 상태)
+void depth_first_search(int init_state, int goal_state);
+
+////////////////////////////////////////////////////////////////////////////////
+int main(int argc, char **argv) {
+    int graph[16][16] = {0,};
+
+    // 인접 행렬 만들기
+    make_adjacency_matrix(graph);
+
+    // 인접 행렬 출력
+    print_graph(graph, 16);
+
+    // .net 파일 만들기 pajek
+    save_graph("pwgc.net", graph, 16);
+
+    //////////////////////////////////////////
+    //depth_first_search( 0, 15); // initial state, goal state
+
+    return 0;
+}
+
+
